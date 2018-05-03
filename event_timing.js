c18682482f5edc62340ce86b2e2b535fb10c3a06
Liquan(Max) Gu <maxlg@chromium.org>
2018-05-03 14:16:07 -0400

    try putting iframe codes in mainframe code, but still failed

--- event_timing.js
+++ event_timing.js
@@ -37,14 +37,16 @@
   window.addEventListener("message", (event) => {
     const id = parseInt(event.data.frameId);
     iframes[id].iframe.remove();
-    iframes[id].entry.nextPaint = event.data.fp - performance.timeOrigin;
+    iframes[id].entry.nextPaint = event.data.firstPaint - performance.timeOrigin;
     delete iframes[id];
+    cntIframe--;
   }, false);
 
   window.requestAnimationFrame(logPaintTimes);
 
   const iframes = {};
   let frameId = 0;
+  let cntIframe = 0;
   function addOrCoalesceEntry(e, newEntryData) {
     window.requestIdleCallback(rIC);
     const hash = eventHash(e);
@@ -58,8 +60,26 @@
       // time in the idle callback.
       entry.processingEnd = newEntryData.processingEnd;
     }
+    if (cntIframe >= 1) return entry;
     const iframe = document.createElement('iframe');
-    iframe.src = 'nextpaint_childframe.html';
+    // iframe.src = 'nextpaint_childframe.html';
+    iframe.srcdoc=
+    '<body>' +
+    '</body>' +
+    '<script>' +
+    '    new PerformanceObserver((entryList, observer)=>{' +
+    '    const es = entryList.getEntriesByName("first-paint");' +
+    '    if (es.length === 0) return;' +
+    '    observer.disconnect();' +
+    '    top.postMessage({' +
+    '      "firstPaint": es[0].startTime + performance.timeOrigin,' +
+    '      "frameId": window.name' +
+    '    }, "*");' +
+    '  }).observe({entryTypes: ["paint"]});' +
+    '  const div = document.createElement("div");' +
+    '  div.innerHTML = window.name;' +
+    '  document.body.appendChild(div);' +
+    '</script>';
     // When width and height are too small, iframe are not painted.
     iframe.style.width = "40px";
     iframe.style.height = "30px";
@@ -68,6 +88,7 @@
     iframes[frameId] = {iframe, entry};
     frameId++;
     document.body.appendChild(iframe);
+    cntIframe++;
     // Record 4 more paintTimes ahead.
     cntPaintsToRecort += 4;
     return entry;
